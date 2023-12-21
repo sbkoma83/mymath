163b2b2bc56328e731418def959291c0569e0a03
sbkoma83 <140725063+sbkoma83@users.noreply.github.com>
2023-12-21 20:18:12 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,62 +6,62 @@ import "math"
 func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
-func add(x, y float64) float64 {
+func Add(x, y float64) float64 {
 	return x + y
 }
 
-func subtract(x, y float64) float64 {
+func Subtract(x, y float64) float64 {
 	return x - y
 }
 
-func multiply(x, y float64) float64 {
+func Multiply(x, y float64) float64 {
 	return x * y
 }
 
-func divide(x, y float64) float64 {
+func Divide(x, y float64) float64 {
 	return x / y
 }
 
-func floor(x float64) float64 {
+func Floor(x float64) float64 {
 	return math.Floor(x)
 }
 
-func ceil(x float64) float64 {
+func Ceil(x float64) float64 {
 	return math.Ceil(x)
 }
 
-func round(x float64) float64 {
+func Round(x float64) float64 {
 	return math.Round(x)
 }
 
-func trunc(x float64) float64 {
+func Trunc(x float64) float64 {
 	return math.Trunc(x)
 }
 
-func maximum(x, y float64) float64 {
+func Maximum(x, y float64) float64 {
 	return math.Max(x, y)
 }
 
-func minimum(x, y float64) float64 {
+func Minimum(x, y float64) float64 {
 	return math.Min(x, y)
 }
 
-func power(x, y float64) float64 {
+func Power(x, y float64) float64 {
 	return math.Pow(x, y)
 }
 
-func squareRoot(x float64) float64 {
+func SquareRoot(x float64) float64 {
 	return math.Sqrt(x)
 }
 
-func sine(x float64) float64 {
+func Sine(x float64) float64 {
 	return math.Sin(x)
 }
 
-func cosine(x float64) float64 {
+func Cosine(x float64) float64 {
 	return math.Cos(x)
 }
 
-func tangent(x float64) float64 {
+func Tangent(x float64) float64 {
 	return math.Tan(x)
 }
